7f96ae1d4160d9122e458e59980d05475fda3485
deepak <deepak@kreatio.com>
2018-10-21 13:00:16 +0530

    Updated documentation

--- src/rx-stomp-rpc-config.ts
+++ src/rx-stomp-rpc-config.ts
@@ -12,11 +12,12 @@ export type setupReplyQueueFnType = (replyQueueName: string, rxStomp: RxStomp) =
  */
 export class RxStompRPCConfig {
   /**
-   * Name of the reply queue
+   * Name of the reply queue. See the guide for details.
+   * Default `/temp-queue/rpc-replies` suitable for RabbitMQ and ActiveMQ.
    */
   replyQueueName?: string;
   /**
-   * Setup the reply queue
+   * Setup the reply queue. See the guide for details.
    */
   setupReplyQueue?: setupReplyQueueFnType;
 }
